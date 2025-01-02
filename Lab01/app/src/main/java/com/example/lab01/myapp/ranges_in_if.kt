23a71a7a1183a72715aadaa3fac572252dd9c68c
hvthang-17 <hvthang1705@gmail.com>
2025-01-02 23:38:54 +0700

    Add Lab01 project

---
+++ Lab01/app/src/main/java/com/example/lab01/myapp/ranges_in_if.kt
@@ -0,0 +1,9 @@
+package com.example.lab01.myapp
+
+fun main(){
+    val numberOfStudents = 50
+    if (numberOfStudents in 1..100) {
+        println(numberOfStudents)
+    }
+
+}
---
+++ Lab01/app/src/main/java/com/example/lab01/myapp/vidu1.kt
@@ -0,0 +1,19 @@
+package com.example.lab01.myapp
+
+fun main()
+{
+    println("xin chao tat ca")
+    println("hom nay troi nhe len cao")
+    println("toi buon khong hieu vi sao toi buon")
+    println("Trời hôm nay đẹp quá")
+    var a : Long = 100L
+    println("so a la" + a)
+    fun prinhello()
+    {
+        println("day la ham hien thi hello")
+    }
+
+    prinhello()
+
+
+}
